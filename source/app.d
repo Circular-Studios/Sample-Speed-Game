02b0d4f9b0edb720b65068beca24d7a13cc1ccc5
Colden Cullen <ColdenCullen@gmail.com>
2014-02-18 11:45:03 -0500

    Moved chat app from speed repo.

--- source/app.d
+++ source/app.d
@@ -1,6 +1,50 @@
-import std.stdio;
+import speed;
+import speed.webconnection;
+
+import std.stdio, std.array;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	write( "Enter IP address to connect to: " );
+	string ipToConnect = readln();
+
+	write( "Will you be hosting this match? (y/n) " );
+	bool hosting = readln()[0] == 'y';
+
+	writeln( hosting );
+	
+	auto con = Connection.open( ipToConnect, hosting, ConnectionType.TCP );
+	
+	write( "Enter your username: " );
+	string username = readln();
+	
+	con.login( username, ConnectionType.TCP );
+	
+	con.onRecieveText ~= msg => write( "Server says: ", msg );
+	
+	while( true )
+	{
+		write( "Message: " );
+		string message = readln();
+		
+		if( message.length > 3 && message[ 0..4 ] == "EXIT" )
+		{
+			con.logoff( username, ConnectionType.TCP );
+			con.close();
+			return;
+		}
+		else if( message.length && message[ 0 ] == '/' )
+		{
+			auto mess = message.split( " " );
+			
+			// Split into /target message
+			con.whisper( mess[ 0 ][ 1..$ ], mess[ 1..$ ].join( " " ), ConnectionType.TCP );
+		}
+		else
+		{
+			con.send!string( message, ConnectionType.TCP );
+		}
+		
+		con.update();
+	}
 }
