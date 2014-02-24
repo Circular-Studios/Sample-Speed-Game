5d766d33f6dc7e8443e2bdf27686b8d9d761454d
Sean Brennan <esean17@yahoo.com>
2014-02-24 14:38:07 -0500

    Added a turn function in app.d, and fixed the move check in TicTacToe.

--- source/app.d
+++ source/app.d
@@ -3,39 +3,55 @@ import tictactoe;
 import speed;
 import speed.webconnection;
 
-import std.stdio, std.array;
+import std.stdio, std.array, std.conv;
+
+Player me;
+
+TicTacToe game;
+
+void turn( shared Connection conn )
+{
+	write("Where would you like to go?\nX: ");
+	int x = readln().to!int;
+	write("Y: ");
+	int y = readln().to!int;
+	
+	Move m = Move(x, y, me);
+	
+	if( game.makeMove( m ) )
+		conn.send!Move( m, ConnectionType.TCP );
+	else writeln( "THATS NOT VALID DUMMY" );
+}
 
 void main()
 {
 	write( "Enter IP address to connect to: " );
 	string ipToConnect = readln();
-
+	
 	write( "Will you be hosting this match? (y/n) " );
 	bool hosting = readln()[0] == 'y';
 	
 	shared Connection con = Connection.open( ipToConnect, hosting, ConnectionType.TCP );
 	writeln( "Connected" );
-
+	
+	// instantiate ticTacToe
+	game = new TicTacToe();
+	
 	if( hosting )
 	{
-		con.onRecieveData!float ~= f => writeln( "Recieved float: ", f );
-		con.onRecieveData!string ~= f => writeln( "Recieved string: ", f );
+		me = Player.X;
 	}
 	else
 	{
-		import core.thread;
-		Thread.sleep( dur!"seconds"( 1 ) );
-
-		con.send!float( 4.3f, ConnectionType.TCP );
-
-		writeln( "Sent float." );
-
-		Thread.sleep( dur!"seconds"( 1 ) );
-
-		con.send!string( "Testing!!!", ConnectionType.TCP );
-
-		writeln( "Sent message." );
+		me = Player.O;
+		turn( con );
 	}
-
+	
+	con.onRecieveData!Move ~= ( Move m )
+	{
+		game.makeMove( m );
+		turn( con );
+	};
+	
 	while( true ) con.update();
 }
--- source/tictactoe.d
+++ source/tictactoe.d
@@ -9,7 +9,7 @@ struct Move
 	int x;
 	int y;
 	Player player;
-
+	
 	@property bool valid()
 	{
 		return x > 0 && x < 4 && y > 0 && y < 4;
@@ -19,21 +19,28 @@ struct Move
 class TicTacToe
 {
 public:
+	this()
+	{
+		foreach( ref row; board ) 
+			foreach( ref cell; row )
+				cell = Player.Empty;
+	}
+	
 	bool makeMove( Move move )
 	{
-		if( !move.valid )
+		if( !move.valid || board[ move.x ][ move.y ] != Player.Empty )
 			return false;
-
+		
 		board[ move.x ][ move.y ] = move.player;
-
+		
 		return true;
 	}
-
+	
 	void print()
 	{
-
+		
 	}
-
+	
 private:
 	Player[3][3] board;
 }
