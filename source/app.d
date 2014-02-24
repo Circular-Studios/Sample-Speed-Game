d2e2f3aa4b166a3c1ff80c1dc82a3a8eadbcc141
Colden Cullen <ColdenCullen@gmail.com>
2014-02-24 13:56:03 -0500

    Pulled out all commented code.

--- source/app.d
+++ source/app.d
@@ -1,9 +1,10 @@
+module app;
+import tictactoe;
 import speed;
 import speed.webconnection;
 
 import std.stdio, std.array;
 
-//*
 void main()
 {
 	write( "Enter IP address to connect to: " );
@@ -37,53 +38,4 @@ void main()
 	}
 
 	while( true ) con.update();
-
-	/*
-	write( "Enter your username: " );
-	string username = readln();
-	
-	con.login( username, ConnectionType.TCP );
-	
-	con.onRecieveText ~= msg => write( "Server says: ", msg );
-	
-	while( true )
-	{
-		write( "Message: " );
-		string message = readln();
-		
-		if( message.length > 3 && message[ 0..4 ] == "EXIT" )
-		{
-			con.logoff( username, ConnectionType.TCP );
-			con.close();
-			return;
-		}
-		else if( message.length && message[ 0 ] == '/' )
-		{
-			auto mess = message.split( " " );
-			
-			// Split into /target message
-			con.whisper( mess[ 0 ][ 1..$ ], mess[ 1..$ ].join( " " ), ConnectionType.TCP );
-		}
-		else
-		{
-			con.send!string( message, ConnectionType.TCP );
-		}
-		
-		con.update();
-	}
-	*/
 }
-/*/
-
-class Test1
-{
-	int x;
-}
-
-void main()
-{
-	import std.traits;
-	auto t = ClassInfo.find( fullyQualifiedName!Test1 );
-	writeln( Test1.stringof, ": ", t );
-}
-//*/
